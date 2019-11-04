13086985bf8356d67bfeb59b28dc192a7337dcd6
Christian Rasmussen <christian.rasmussen@me.com>
2019-11-04 03:20:17 +0100

    Put module name in an attribute for tests

--- test/mutable_storage_test.exs
+++ test/mutable_storage_test.exs
@@ -2,16 +2,18 @@ defmodule MutableStorageTest do
   use ExUnit.Case
   doctest MutableStorage
 
+  @sut_module MutableStorage
+
   test "buffer" do
-    ref = MutableStorage.buffer_new(1)
-    assert MutableStorage.buffer_set_byte(ref, 0, 42) == :ok
-    assert MutableStorage.buffer_get_byte(ref, 0) == 42
+    ref = @sut_module.buffer_new(1)
+    assert @sut_module.buffer_set_byte(ref, 0, 42) == :ok
+    assert @sut_module.buffer_get_byte(ref, 0) == 42
   end
 
   test "term" do
-    ref = MutableStorage.term_new(42)
-    assert MutableStorage.term_get(ref) == 42
-    assert MutableStorage.term_set(ref, 43) == :ok
-    assert MutableStorage.term_get(ref) == 43
+    ref = @sut_module.term_new(42)
+    assert @sut_module.term_get(ref) == 42
+    assert @sut_module.term_set(ref, 43) == :ok
+    assert @sut_module.term_get(ref) == 43
   end
 end
