cb97b7bc57ccc3e33f44c219a0b36f816310a585
Christian Rasmussen <christian.rasmussen@me.com>
2019-11-17 00:24:38 +0100

    Reorganize tests

--- test/mutable_storage_test.exs
+++ test/mutable_storage_test.exs
@@ -4,16 +4,16 @@ defmodule MutableStorageTest do
 
   @sut_module MutableStorage
 
-  test "buffer" do
-    ref = @sut_module.buffer_new(1)
-    assert @sut_module.buffer_set_byte(ref, 0, 42) == :ok
-    assert @sut_module.buffer_get_byte(ref, 0) == 42
-  end
-
   test "term" do
     ref = @sut_module.term_new(42)
     assert @sut_module.term_get(ref) == 42
     assert @sut_module.term_set(ref, 43) == :ok
     assert @sut_module.term_get(ref) == 43
+  end
+
+  test "buffer byte with offset=0" do
+    ref = @sut_module.buffer_new(1)
+    assert @sut_module.buffer_set_byte(ref, 0, 42) == :ok
+    assert @sut_module.buffer_get_byte(ref, 0) == 42
   end
 end
